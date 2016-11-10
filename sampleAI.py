d410657db33b45ac38853efc05240d5813069e0d
Ryan Stillings <rystills@gmail.com>
2016-11-09 20:10:01 -0500

    Added sample AI and eclipse gitignore
    Added sample AI from aicomp/docs for reference, and modified gitignore
    to include eclipse project files

---
+++ sampleAI.py
@@ -0,0 +1,19 @@
+import time
+import requests # if not installed already, run python -m pip install requests OR pip install requests, whatever you normally do
+import random
+r = requests.post('http://aicomp.io/api/games/search', data={'devkey': "devkey", 'username': 'username'}) # search for new game
+json = r.json() # when request comes back, that means you've found a match! (validation if server goes down?)
+print(json)
+gameID = json['gameID']
+playerID = json['playerID']
+print(gameID)
+print(playerID)
+possibleMoves = ['mu', 'ml', 'mr', 'md', 'tu', 'tl', 'tr', 'td', 'b', '', 'op', 'bp', 'buy_count', 'buy_range', 'buy_pierce', 'buy_block']
+output = {'state': 'in progress'}
+while output['state'] != 'complete':
+    randomInt = random.randint(0,len(possibleMoves)-1)
+    r = requests.post('http://aicomp.io/api/games/submit/' + gameID, data={'playerID': playerID, 'move': possibleMoves[randomInt], 'devkey': "devkey"}); # submit sample move
+    json = r.json()
+    print(json)
+    output = json
+    
