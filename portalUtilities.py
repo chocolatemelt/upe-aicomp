f2f1cb2079d005310730b276df585c0c5a84ae53
Ryan Stillings <rystills@gmail.com>
2016-12-01 16:49:18 -0500

    Fixed runtime errors in portal functions
    canTraversePortal, portalExitSpace, and getComplementaryPortal no longer
    generate runtime errors when called

--- portalUtilities.py
+++ portalUtilities.py
@@ -19,18 +19,18 @@ def getAdjacentSpaces(board,space,direction="all"):
         return None if len(adjacentSpaces) == 0 else adjacentSpaces[0] if direction != "all" else adjacentSpaces
     
     
-def getComplementaryPortal(gameState,entrancePortal):
+def getComplementaryPortal(gameState,entrancePortalCoord):
     """return the portal corresponding to the entrance portal, if it exists"""
     portalKeys = gameState['portalMap'].keys()
     for coord in portalKeys:
-        if (gameState['portalMap'][coord]['owner'] == gameState['portalMap'][entrancePortal]['owner']) and (gameState['portalMap'][coord]['portalColor'] != gameState['portalMap'][entrancePortal]['portalColor']):
+        if (gameState['portalMap'][coord]['owner'] == gameState['portalMap'][entrancePortalCoord]['owner']) and (gameState['portalMap'][coord]['portalColor'] != gameState['portalMap'][entrancePortalCoord]['portalColor']):
             return coord
     return None   
 
 def portalExitSpace(board,gameState,entranceSpace):
     """return the space that the portal contained in entranceSpace leads to"""
-    if (entranceSpace.containsPortal):
-        correspondingPortal = getComplementaryPortal(gameState,entranceSpace.containedPortal)
+    if (entranceSpace.containsPortal and gameState['portalMap'][entranceSpace.containedPortalCoord] != {}):
+        correspondingPortal = getComplementaryPortal(gameState,entranceSpace.containedPortalCoord)
         if (correspondingPortal == None):
             return None
         correspondingColor = gameState['portalMap'][correspondingPortal]['portalColor']
@@ -41,12 +41,15 @@ def portalExitSpace(board,gameState,entranceSpace):
 
 def canTraversePortal(board,gameState,currentSpace,newSpace):
     """return whether or not newSpace contains a portal facing currentSpace, which leads to a walkable space"""
-    if (newSpace.containsPortal()):
-        correspondingColor = gameState['portalMap'][newSpace.containedPortal]['portalColor']
-        playerString = "player" if (gameState['portalMap'][newSpace.containedPortal]['owner'] == gameState['playerIndex']) else "opponent"
-        direction = gameState[playerString][correspondingColor]['direction']
-        adjacentSpace = getAdjacentSpaces(board,newSpace.containedPortal,direction)
-        if (not (adjacentSpace == currentSpace)):
-            return False,None
-        exitSpace = portalExitSpace(gameState,newSpace)
-        return (exitSpace != None and exitSpace.type == SpaceType.empty),exitSpace
+    if (newSpace.containsPortal and gameState['portalMap'][newSpace.containedPortalCoord] != {}):
+        #print(gameState['portalMap'])
+        #todo: didn't realize that key of portalMap is portal direction, so we can eliminate direction finding code and simply use this key
+        #print(gameState['portalMap'][newSpace.containedPortalCoord])
+        correspondingColor = list(gameState['portalMap'][newSpace.containedPortalCoord].values())[0]['portalColor']
+        playerString = "player" if (list(gameState['portalMap'][newSpace.containedPortalCoord].values())[0]['owner'] == gameState['playerIndex']) else "opponent"
+        direction = gameState[playerString][correspondingColor+"Portal"]['direction']
+        adjacentSpace = getAdjacentSpaces(board,newSpace.containedPortalCoord,direction)
+        if (adjacentSpace == currentSpace):
+            exitSpace = portalExitSpace(gameState,newSpace)
+            return (exitSpace != None and exitSpace.type == SpaceType.empty),exitSpace
+    return (False,None)
